e87abf0cafd38e60a945f929a68354bf94bc2427
andikaputral <andikaputralikha@gmail.com>
2024-09-21 14:19:48 +0700

    layout fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,9 +13,8 @@
 
 <div class="min-h-full">
     <x-header></x-header>
-
     <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div class="mt-32 mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
             {{ $slot }}
         </div>
